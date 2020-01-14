b30c37bde78555dace7ade086c6b9e2cf960aeb1
zat.圈 <quan@qq.com>
2020-01-14 16:04:15 +0800

    试用模板变量

--- app/controllers/index.ts
+++ app/controllers/index.ts
@@ -1,11 +1,16 @@
 import * as path from "path";
 import * as cons from "consolidate";
-import { Get, Controller } from "routing-controllers";
-
+import { Get, Controller, Ctx } from "routing-controllers";
+import { Context } from 'koa'
+import bundle from "../../web/src/bundle";
 @Controller()
 export default class {
-  @Get("/")
-  async router() {
-    return cons.ejs(path.resolve(__dirname, "../views/index.ejs"), {});
+  @Get("/*")
+  async router(@Ctx() ctx: Context) {
+    const pathUrl = ctx.path
+    console.log(pathUrl)
+    return cons.ejs(path.resolve(__dirname, "../views/index.ejs"), {
+      content: bundle.serverRender({path: pathUrl})
+    });
   }
 }
