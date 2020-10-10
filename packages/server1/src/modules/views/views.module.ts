41e4d63737a941b1da922fc8b6534be9ca77c8da
zeng <zenglbg@gmail.com>
2020-10-10 12:11:36 +0800

    删除next

--- packages/server1/src/modules/views/views.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ViewsController } from "./views.controller";
-
-@Module({
-  imports: [],
-  controllers: [ViewsController],
-})
-export class ViewsModule {}
