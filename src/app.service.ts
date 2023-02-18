5944c47a771772ef7a3c124c38a97a7eb348945c
zsh <zsh@qq.com>
2023-02-18 04:39:49 +0000

    nginx

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! \n 你是个嗦嗨';
   }
 }
