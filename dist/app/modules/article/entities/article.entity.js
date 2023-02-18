015a2c9aa7a36a0d0aa0bba2a5594e9c29baca1b
zsh <zsh@qq.com>
2023-02-18 09:03:33 +0000

    去除dist

--- dist/app/modules/article/entities/article.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Article = void 0;
-class Article {
-}
-exports.Article = Article;
-//# sourceMappingURL=article.entity.js.map
