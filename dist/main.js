67e96d7b5c5f967846ab4aea65140f3ccd59677a
zsh <zsh@qq.com>
2023-02-18 14:21:11 +0700

    允许构建文件上传

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const PORT = 3000;
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(PORT);
+    console.log(`
+  
+    http://localhost:${PORT}
+
+    
+  `);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
