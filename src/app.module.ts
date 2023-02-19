e4eaa00b4a59de10e9289bd39d96af141c2c8c5a
zsh <zsh@qq.com>
2023-02-19 09:42:41 +0000

    添加chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app/app.service';
 // import { PrismaService } from '../prisma.service';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
+import { ChatModule } from './modules/chat/chat.module';
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [UserModule, PostModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
